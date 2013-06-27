a960cfe71af448465552abaf3579d095345bee80
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2013-06-27 15:39:27 +0800

    upgrade to twitter api v1.1

--- common/utils.py
+++ common/utils.py
@@ -1,5 +1,9 @@
 import urllib2, sys, sets
 import json
+from mongomodel.crawl.twitter.models import ScreenNameCache,init
+import pymongo
+
+from twython import Twython, TwythonError
 
 
 MAX_TERM_ALLOWED = 75
@@ -20,40 +24,75 @@ def read_cred(file):
 
 URL = "https://api.twitter.com/1/users/lookup.json?include_entities=true&screen_name="
 
-def get_userids(usernames):
+def get_userids(cred,usernames):
+    print "usernames" + str(usernames)
     batches = split_list_by(usernames, MAX_TERM_ALLOWED)
     userids = []
+    notfound = []
     for batch in batches:
-        url = URL+','.join(batch)
-        print url
-        f = urllib2.urlopen(url)
-        j = json.loads(f.read())
-        userids = userids + map(lambda x:x['id'] ,j)
-        found = sets.Set(map(lambda x:x['screen_name'].lower(), j))
-        print "missing" + str(sets.Set(map(lambda x:x.lower(), batch)) - found)
-        f.close()
+        #url = URL+','.join(batch)
+        #print url
+        try:
+            #f = urllib2.urlopen(url)
+            #j = json.loads(f.read())
+            twitter = Twython(cred['consumer_key'], cred['consumer_secret'],
+                              cred['access_token_key'], cred['access_token_secret'])
+            j = twitter.lookup_user(screen_name=batch)
+            userids = userids + map(lambda x:x['id'] ,j)
+            found = sets.Set(map(lambda x:x['screen_name'].lower(), j))
+            for x in j:
+                print "caching"
+                print ((x['screen_name'], x['id']))
+                cache_userid(x['screen_name'], x['id'])
+            # f.close()
+        except Exception as e:
+            print "connection refused" + str(e)
+            found = sets.Set([])
+        notfound_this_round = sets.Set(map(lambda x:x.lower(), batch)) - found
+        print "missing" + str(notfound_this_round)
+        notfound = notfound + list(notfound_this_round)
     print userids
-    return userids
+    return (userids, notfound)
 
-def get_userids_file(infile):
+def get_cached_userids(usernames):
+    found = []
+    not_found = []
+    for n in usernames:
+        cs = ScreenNameCache.objects.filter(screenname = n)
+        if len(cs) > 0:
+            c = cs[0]
+            found.append(c.tid)
+        else:
+            not_found.append(n)
+    return { 'found' : found, 'not_found' : not_found }
+
+def cache_userid(name,id):
+    c = ScreenNameCache(screenname = name, tid = id)
+    c.save()
+
+def get_userids_file(cred,infile):
     inh = open(infile,'r')
     unames = []
     for ln in inh:
-        s = ln.strip('\r\n"')
+        s = ln.strip('\r\n"').replace(' ','%20')
         unames.append(s)
     inh.close()
-    uids = get_userids(unames)
-    return uids
-
-
-def get_terms_file(infile):
-    inh = open(infile,'r')
-    terms = []
-    for ln in inh:
-        s = ln.strip('\r\n"')
-        terms.append(s.lower())
-    inh.close()
-    return terms
+    db = init()
+    cache_result = get_cached_userids(unames)
+    found = cache_result['found']
+    not_found = cache_result['not_found']
+    print "found %d in cache" % (len(found))
+    print "looking for %d from API" % (len(not_found))
+    (uids,not_found_still) = get_userids(cred,not_found)
+    ofh = open(infile + '.notfound', 'w')
+    for n in not_found_still:
+        print >> ofh, n
+    ofh.close()
+    if hasattr(db,'disconnect'):
+        db.disconnect()
+    else:
+        db.connection.disconnect()
+    return (found+uids)
 
 
 def split_list_by(l,n):
@@ -63,3 +102,4 @@ def split_list_by(l,n):
         if i*n < len(l):
             result.append(l[i*n:(i+1)*n])
     return result
+
--- moodscot.py
+++ moodscot.py
@@ -7,23 +7,23 @@ from espeak import espeak # apt-get install python-espeak espeak
 from datetime import datetime
 from time import sleep
 
-import piface.pfio
-
-#STREAM_URL = "https://stream.twitter.com/1/statuses/filter.json?follow="
-STREAM_URL = "https://stream.twitter.com/1/statuses/filter.json?track="
-
-
 def speak(text):
     t = re.sub(r"http://[^ ]*", "", text)
     t = re.sub(r"@", "", t)
     t = re.sub(r"#", "", t)
     espeak.synth(t)
 
+'''
+import piface.pfio
+
 def move(unit):
     piface.pfio.digital_write(1, 1)
     sleep(max(1,unit/4))
     piface.pfio.digital_write(1, 0)
 
+
+piface.pfio.init()
+'''
 
 def speak_and_move(text):
     speak(text)
@@ -32,51 +32,38 @@ def speak_and_move(text):
 
 def act(data):
     try:
-        data_json = json.loads(data)
-        if data_json.has_key("text"):
+        if data.has_key("text"):
             # todo get the mood
-            text = data_json["text"]
-            speak_and_move
+            text = data["text"]
+            # speak_and_move
             print "%s" % text
     except ValueError,e:
         pass
 
-def on_receive(data):
-    #data_json = json.loads(data)
-    #print data    
-    act(data)
 
 if len(sys.argv) < 1:
-    print "Usage: stream.py <cred_file> <terms_file>"
+    print "Usage: python moodscot.py <cred_file> <terms>"
     sys.exit(1)
 
 espeak.set_voice("f1") # female
 piface.pfio.init()
 
-#user_ids = get_userids_file(sys.argv[2])
-'''
-terms = get_terms_file(sys.argv[2])
+
+cred = read_cred(sys.argv[1])
+
+class MyStreamer(TwythonStreamer):
+    def on_success(self, data):
+	act(data)
+
+    def on_error(self, status_code, data):
+        print status_code, data
 
 
-def loop(retry):
-    try:
-        cred = read_cred(sys.argv[1])
-        conn = pycurl.Curl()
-        conn.setopt(pycurl.USERPWD, "%s:%s" % (cred['user'], cred['password']))
-        # conn.setopt(pycurl.URL, STREAM_URL+','.join(map(str,user_ids)))
-        conn.setopt(pycurl.URL, STREAM_URL+','.join(map(str,terms)))
-        conn.setopt(pycurl.WRITEFUNCTION, on_receive)
-        conn.perform()
-    except:
-        if retry > 0:
-            print "="*20
-            print "retrying..."
-            loop(retry-1)
-        else:
-            print "="*20            
-            print "no more retry, exiting..."
-            sys.exit(0)
+stream = MyStreamer(cred['consumer_key'], cred['consumer_secret'],
+                    cred['access_token_key'], cred['access_token_secret'])
 
-loop(5)
-'''
+
+keywords = sys.argv[2]
+stream.statuses.filter(track=keywords)
+
 
